4c58b245fcc4b07897cbfb9db28282c0de165862
Nyrk19 <127149633+Nyrk19@users.noreply.github.com>
2023-06-05 21:55:03 -0600

    Interfaz completa para compilador

--- Practica_Compi.py
+++ Practica_Compi.py
@@ -1,8 +1,12 @@
 import tkinter as tk
+from tkinter import ttk
 
 def guardar_texto():
-    texto = texto_entry.get("1.0", tk.END)  # Obtenertodo el texto
-    print(texto)
+    texto = texto_entry.get("1.0", tk.END)  # Obtener todo el texto
+    mensaje_text.config(state=tk.NORMAL)  # Habilitar la edición del segundo campo de texto
+    mensaje_text.delete("1.0", tk.END)  # Borrar el contenido anterior del mensaje
+    mensaje_text.insert(tk.END, texto)  # Agregar el nuevo mensaje
+    mensaje_text.config(state=tk.DISABLED)  # Volver a deshabilitar la edición del segundo campo de texto
 
 # Crear la ventana principal
 ventana = tk.Tk()
@@ -19,12 +23,29 @@ marco.pack(pady=20)
 texto_scrollbar = tk.Scrollbar(marco)
 texto_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-texto_entry = tk.Text(marco, height=int(ventana.winfo_height()*.055), width=int(ventana.winfo_width()*.12))  # Ajustar el tamaño del campo de texto
+texto_entry = tk.Text(marco, height=int(ventana.winfo_height()*.031), width=int(ventana.winfo_width()*.12))  # Ajustar el tamaño del campo de texto
 texto_entry.pack(pady=(0, 10), ipady=5, ipadx=5, padx=5, anchor="w")  # Ajustar los parámetros para reducir el espacio
 
 # Vincular el desplazamiento del texto con la scrollbar
 texto_entry.config(yscrollcommand=texto_scrollbar.set)
 texto_scrollbar.config(command=texto_entry.yview)
+
+# Crear un marco para contener el segundo campo de texto y el scrollbar
+marco_mensaje = tk.Frame(ventana)
+marco_mensaje.pack(pady=20)
+
+# Crear el segundo campo de texto con desplazamiento (scrollbar)
+mensaje_scrollbar = tk.Scrollbar(marco_mensaje)
+mensaje_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+mensaje_text = tk.Text(marco_mensaje, height=int(ventana.winfo_height()*.02), width=int(ventana.winfo_width()*.12), relief="solid", borderwidth=1)
+mensaje_text.pack(side=tk.LEFT, fill=tk.BOTH)
+
+mensaje_scrollbar.config(command=mensaje_text.yview)
+mensaje_text.config(yscrollcommand=mensaje_scrollbar.set)
+
+# Deshabilitar la edición del segundo campo de texto
+mensaje_text.config(state=tk.DISABLED)
 
 # Crear un botón para guardar el texto
 guardar_button = tk.Button(ventana, text="Compilar", command=guardar_texto)
